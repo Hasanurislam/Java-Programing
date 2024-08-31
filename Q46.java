ec4d5426835cde2dd06275e96c19896549d98f20
hasanurislam <hasanur7811@gmail.com>
2024-08-31 21:02:35 +0530

    Find the smallest element in the array

---
+++ Q46.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Q46 {
+    public static void main(String[] args) {
+        int max;
+        System.out.print("Enter the array elements: ");
+        Scanner r=new Scanner(System.in);
+        int a[]=new int[5];
+        for(int i=0;i<a.length;i++)
+        {
+           a[i]=r.nextInt();
+        }
+        max=a[0];
+        for(int i=1;i<a.length;i++)
+        {
+            if(a[i]<max)
+            {
+                max=a[i];
+            }
+            }
+            System.out.print("The biggest element in the array: "+max);
+
+
+        }
+    }
