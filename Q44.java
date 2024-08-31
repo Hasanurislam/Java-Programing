245055fa3b1536efb5c7e137e6dceceb1e7b56a6
hasanurislam <hasanur7811@gmail.com>
2024-08-31 20:44:08 +0530

    Array compare

---
+++ Q44.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class Q44 {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,45};
+        int b[]={10,20,30,40,45};
+
+        if(Arrays.equals(a,b))
+        {
+            System.out.print("Arrays are equal");
+        }
+        else
+        {
+            System.out.print("The arrays are not equal");
+        }
+    }
+    
+}
