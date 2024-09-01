b1b8f84bee98a629d4bdb163170b48826b697cfa
hasanurislam <hasanur7811@gmail.com>
2024-09-01 13:06:43 +0530

    Star Pattern

---
+++ Q49.java
@@ -0,0 +1,13 @@
+public class Q49 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)//rows
+        {
+            for(int j=1;j<=i;j++)//column
+            {
+               System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
