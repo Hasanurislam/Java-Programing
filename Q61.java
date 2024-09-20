dc62aa4e186ad1286bf2cac63245a75a21725e04
hasanurislam <hasanur7811@gmail.com>
2024-09-20 21:54:41 +0530

    Methods in java

---
+++ Q61.java
@@ -0,0 +1,11 @@
+public class Q61 {
+    public static void main(String[] args) {
+        Q61 r=new Q61();
+       r.disp();
+    }
+    
+    void disp()
+    {
+        System.out.print("Hasanur");
+    }
+}
