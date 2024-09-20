cdb30304bb10f33a84d4b6256f81efcaa020c900
hasanurislam <hasanur7811@gmail.com>
2024-09-20 22:14:10 +0530

    Methods

--- Q61.java
+++ Q61.java
@@ -1,11 +1,19 @@
 public class Q61 {
+    public static void add(){
+        int a=10,b=20;
+        System.out.print("sum of "+a +" and "+b +" = "+(a+b));
+    }
     public static void main(String[] args) {
         Q61 r=new Q61();
        r.disp();
+       r.disp();
+       r.disp();
+       r.disp();
+       add();
     }
     
     void disp()
     {
-        System.out.print("Hasanur");
+        System.out.print("Hasanur\n");
     }
 }
