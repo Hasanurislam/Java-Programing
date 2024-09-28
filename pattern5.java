7739cbbd8bd23c198bffd35612fedd3ae831b220
hasanurislam <hasanur7811@gmail.com>
2024-09-28 16:28:18 +0530

    half pryamid with number with proper space

--- pattern5.java
+++ pattern5.java
@@ -7,7 +7,7 @@ public class pattern5 {
             //inner loop
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.print("\n");
         }
