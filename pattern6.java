35ecdd600894030dc5d49c8147ace0e834ee5460
hasanurislam <hasanur7811@gmail.com>
2024-09-28 16:44:16 +0530

    another technique

--- pattern6.java
+++ pattern6.java
@@ -2,14 +2,15 @@ public class pattern6 {
     public static void main(String[] args) {
         int n=5;
         //outer loop
-        for(int i=n;i>=1;i--)
+        for(int i=1;i<=n;i++)
         {
             //inner loop
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=n-i+1;j++)
             {
                 System.out.print(j+" ");
             }
             System.out.println();
+            
         }
     }
     
