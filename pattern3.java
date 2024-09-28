c1abfc7681a8120b071245bf781565a25538921b
hasanurislam <hasanur7811@gmail.com>
2024-09-28 13:07:25 +0530

    pattern3 reverse

--- pattern3.java
+++ pattern3.java
@@ -1,7 +1,8 @@
 public class pattern3 {
     public static void main(String[] args) {
         //outer loop
-        for(int i=1;i<=4;i++)
+        int n=4;
+        for(int i=n;i>=1;i--)
         {
             //inner loop
             for(int j=1;j<=i;j++)
