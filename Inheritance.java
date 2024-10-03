4ead8e1f58c6b822d11cee8e632369799aa80534
hasanurislam <hasanur7811@gmail.com>
2024-10-03 23:06:15 +0530

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,13 @@
+class shape{
+    String color;
+
+}
+class Tringle extends shape{
+
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Tringle t1=new Tringle();
+        t1.color="red";
+    }
+}
