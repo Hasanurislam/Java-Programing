d16ba8437cdea660a3a323ed4487f74054ae42d8
hasanurislam <hasanur7811@gmail.com>
2024-10-03 23:28:08 +0530

    Hirercal Inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -9,14 +9,16 @@ class Tringle extends shape{
         System.out.println(0.5*l*h);
       }
 }
-class EquallateralTringle extends Tringle{
-    public void area(int l,int h){
-        System.out.println(0.5*l*h);
+class circle extends shape{
+    public void area(int r){
+        System.out.print((3.14)*r*r);
     }
 }
 public class Inheritance {
     public static void main(String[] args) {
         Tringle t1=new Tringle();
+        circle c1=new circle();
         t1.area(5,7);
+        c1.area(5);
     }
 }
