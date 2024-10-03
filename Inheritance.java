71a9ad7532bd5c812e88327ee8072f8f7f79e364
hasanurislam <hasanur7811@gmail.com>
2024-10-03 23:48:03 +0530

    Multi level Inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -14,11 +14,19 @@ class circle extends shape{
         System.out.print((3.14)*r*r);
     }
 }
+class square extends shape{
+    public void area(int side){
+        System.out.println("area of square "+(side*side));
+    }
+}
 public class Inheritance {
     public static void main(String[] args) {
         Tringle t1=new Tringle();
         circle c1=new circle();
+        square s1=new square();
         t1.area(5,7);
         c1.area(5);
+        s1.area(5);
+
     }
 }
