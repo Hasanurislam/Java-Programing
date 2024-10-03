51d1edaa5eb836b4d7cc71e669677946a5ec3cb0
hasanurislam <hasanur7811@gmail.com>
2024-10-03 23:14:21 +0530

    single level inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -1,13 +1,17 @@
 class shape{
-    String color;
+    public void area(){
+        System.out.print("displays");
+    }
 
 }
 class Tringle extends shape{
-
+      public void area(int l,int h){
+        System.out.println(1/2*l*h);
+      }
 }
 public class Inheritance {
     public static void main(String[] args) {
         Tringle t1=new Tringle();
-        t1.color="red";
+        
     }
 }
