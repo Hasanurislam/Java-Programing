cb4a872daac5855774c3cebeb62903aeee8d29dc
hasanurislam <hasanur7811@gmail.com>
2024-10-08 23:28:44 +0530

    practice

---
+++ OOP2.java
@@ -0,0 +1,12 @@
+class Main{
+    final int x=10;
+}
+public class OOP2 {
+    public static void main(String[] args) {
+        Main v1=new Main();
+        //v1.x=20;
+        System.out.println(v1.x);
+
+    }
+    
+}
