2a90ab1d0ea3e171149b1a7dffd64ebbc85fc2c0
hasanurislam <hasanur7811@gmail.com>
2024-10-20 20:28:27 +0530

    class and objects

---
+++ ClassAndobjects.java
@@ -0,0 +1,13 @@
+class Demo{
+    int a=10;String name="Hasanur";
+    void show(){
+        System.out.print(a+"   "+name);
+    }
+}
+public class ClassAndobjects {
+    public static void main(String[] args) {
+        
+        Demo r=new Demo();
+        r.show();
+    }
+}
