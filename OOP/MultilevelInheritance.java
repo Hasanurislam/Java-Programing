d310dccb52e8022fc195a678a5f9dafb269cb1e8
hasanurislam <hasanur7811@gmail.com>
2024-11-21 20:04:03 +0530

    Multilevel inheritance with user input

--- OOP/MultilevelInheritance.java
+++ OOP/MultilevelInheritance.java
@@ -14,7 +14,6 @@ class A{
 }
 class B extends A{
     void sub(){
-        a=10;b=20;
         c=a-b;
         System.out.println("sub of two Number:"+c);
     }
@@ -22,14 +21,12 @@ class B extends A{
 }
 class C extends B{
     void mul(){
-            a=10;b=20;
             c=a*b;
             System.out.println("multiplication of two Number:"+c);
     }
 }
 class D extends C{
     void rem(){
-        a=10;b=20;
         c=a%b;
         System.out.println("remiender of two Number:"+c);
     }
