1bb8ec486c29ac5dce68f3951bf7fc7ece0dd45b
hasanurislam <hasanur7811@gmail.com>
2024-11-21 23:28:26 +0530

    super keyboard

--- OOP/SuperKeybord.java
+++ OOP/SuperKeybord.java
@@ -1,21 +1,30 @@
 class A{
     //int a=10;
-    void show(){
+    /* void show(){
+    System.out.println("Hello1");
+    }
+    */
+
+    A(){
         System.out.println("Hello1");
     }
 }
 class B extends A{
     //int a=30;
-    void show(){
+    /*void show(){
         super.show();
         System.out.println("Hello2");
     }
+        */
+
+        B(){
+            System.out.println("Hello2");
+        }
 
 }
 public class SuperKeybord {
     public static void main(String args[]){
         B r=new B();
-        r.show();
     }
     
 }
