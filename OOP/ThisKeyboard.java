5472ab86e72f070bec82524a29b601df32e38dc8
hasanurislam <hasanur7811@gmail.com>
2024-11-23 23:20:49 +0530

    This keyboard

--- OOP/ThisKeyboard.java
+++ OOP/ThisKeyboard.java
@@ -1,17 +1,25 @@
-class A{
+class A {
     int a;
-    A(int a){
-        this.a=a;
+
+    // Constructor to initialize 'a'
+    A(int a) {
+        this.a = a;
     }
-    void show(){
-        System.out.println(a);
+
+    // Method to display the value and some additional logic
+    void show() {
+        System.out.println("Value of a: " + a);
+        System.out.println("Square of a: " + (a * a));
+        System.out.println("Is a even? " + (a % 2 == 0 ? "Yes" : "No"));
     }
 }
-public class ThisKeyboard {
-    public static void main(String args[]){
-        A r=new A(100);
-        r.show();
 
+public class ThisKeyboard {
+    public static void main(String args[]) {
+        // Creating an object of class A and initializing with 100
+        A r = new A(100);
+
+        // Calling the show method to display the value and additional info
+        r.show();
     }
-    
 }
