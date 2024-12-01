fe910af41d250b74a8094a37b591a5b0081533a3
hasanurislam <hasanur7811@gmail.com>
2024-12-02 00:17:53 +0530

    practice

--- LabP.java
+++ LabP.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 
-class Binary{
-    int num;
-    Binary()
-    {
-        Scanner ob=new Scanner(System.in);
-        System.out.print("Enter Any Number: ");
-        int n=ob.nextInt();
-        this.num=n;
-        String binary=" ";
-        while(n>0)
-        {
-            int rem=n%2;
-            binary=rem +binary;
-            n=n/2;
-        }
-        n=this.num;
-        System.out.print("The binary of "+n+"="+binary);
-    }
-}
 public class LabP {
    
     public static void main(String[] args) {
-       Binary r=new Binary();
+       Scanner ob=new Scanner(System.in);
+       System.out.print("Enter a Number: ");
+       int n=ob.nextInt();
+       int orginal=n;
+       int digit,fact=1,sum=0;
+       while(n>0)
+       {
+        digit=n%10;
+        fact=1;
+        for(int i=1;i<=digit;i++)
+        {
+          fact=fact*i;
+        }
+         sum=sum+fact;
+         n=n/10;
+       }
+       n=orginal;
+       if(sum==n)
+       {
+        System.out.print("it is a krishnamurty number: ");
+       }
+       else{
+        System.out.print("Not a krishnamurty number: ");
+       }
       
     }
 }
