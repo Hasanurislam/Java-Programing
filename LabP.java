aa790d4458b3e49c9d443e74f11fec650d3107fa
hasanurislam <hasanur7811@gmail.com>
2024-12-01 23:35:30 +0530

    practice

--- LabP.java
+++ LabP.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 
+class Swap{
+    int n1,n2;
+    Swap(int c,int d)
+    {
+      this.n1=c;
+      this.n2=d;
+      System.out.println("Before Swaping the value of A= "+this.n1+" B= "+this.n2);
 
+    }
+    Swap(Swap ref)
+    {
+        int temp;
+        temp=ref.n1;
+        ref.n1=ref.n2;
+        ref.n2=temp;
+        System.out.println("After swaping the value of A= "+ref.n1+" B= "+ref.n2);
+    }
+}
 public class LabP {
    
     public static void main(String[] args) {
-        int term;
+        int a,b;
         Scanner ob=new Scanner(System.in);
-        System.out.print("Enter the term: ");
-        term=ob.nextInt();
-        for(int i=1;i<=10;i++)
-        {
-            int m=term*i;
-            System.out.println(term+"X"+i+"="+m);
-        }
-     
+        System.out.println("Enter the value of A: ");
+        a=ob.nextInt();
+        System.out.println("Enter the value of B: ");
+        b=ob.nextInt();
+
+        Swap r1=new Swap(a,b);
+        Swap r2=new Swap(r1);
+        
       
     }
 }
