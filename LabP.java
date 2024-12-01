c86b4980fe215f9999a8d20201e69ea19c830672
hasanurislam <hasanur7811@gmail.com>
2024-12-01 23:50:25 +0530

    practice

--- LabP.java
+++ LabP.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 
-class Swap{
-    int n1,n2;
-    Swap(int c,int d)
+class Binary{
+    int num;
+    Binary()
     {
-      this.n1=c;
-      this.n2=d;
-      System.out.println("Before Swaping the value of A= "+this.n1+" B= "+this.n2);
-
-    }
-    Swap(Swap ref)
-    {
-        int temp;
-        temp=ref.n1;
-        ref.n1=ref.n2;
-        ref.n2=temp;
-        System.out.println("After swaping the value of A= "+ref.n1+" B= "+ref.n2);
+        Scanner ob=new Scanner(System.in);
+        System.out.print("Enter Any Number: ");
+        int n=ob.nextInt();
+        this.num=n;
+        String binary=" ";
+        while(n>0)
+        {
+            int rem=n%2;
+            binary=rem +binary;
+            n=n/2;
+        }
+        n=this.num;
+        System.out.print("The binary of "+n+"="+binary);
     }
 }
 public class LabP {
    
     public static void main(String[] args) {
-        int a,b;
-        Scanner ob=new Scanner(System.in);
-        System.out.println("Enter the value of A: ");
-        a=ob.nextInt();
-        System.out.println("Enter the value of B: ");
-        b=ob.nextInt();
-
-        Swap r1=new Swap(a,b);
-        Swap r2=new Swap(r1);
-        
+       Binary r=new Binary();
       
     }
 }
