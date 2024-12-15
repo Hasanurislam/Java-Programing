dcd3c814ec943daa1abdf466b63f5e8304c8d8c2
hasanurislam <hasanur7811@gmail.com>
2024-12-15 13:18:51 +0530

    pattern

--- Exam1.java
+++ Exam1.java
@@ -1,17 +1,16 @@
 public class Exam1 {
 
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++)//rows
-        {
-            for(int j=i;j<5;j++)
-            {
-                System.out.print(" ");
-            }
-                for(int k=1;k<=i;k++){
-                  System.out.print("*");
-                }
-                System.out.println();
-            
+       for(int i=1;i<=5;i++)
+       {
+        for(int j=1;j<i;j++){
+            System.out.print(" ");
         }
+        for(int k=i;k<=5;k++){
+            System.out.print("*");
+        }
+        System.out.println();
+
+       }
     }
 }
