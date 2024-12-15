fa76ad83d73d3b46b0436ffa1b2e3e99c1f9e5e2
hasanurislam <hasanur7811@gmail.com>
2024-12-15 20:46:57 +0530

    Inplementing Thread using extending thread class

--- Exam1.java
+++ Exam1.java
@@ -1,29 +1,25 @@
+class A extends Thread{
+   public void run(){
+       try{
+         for(int i=1;i<=5;i++){
+         System.out.println("Hasanur");
+         Thread.sleep(1000);
+      }
+   }
+   catch(InterruptedException i){
+
+   }
+}
+}
 public class Exam1 {
 
-    public static void main(String[] args) {
-       for(int i=1;i<=5;i++)//rows
-       {
-         for(int j=i;j<5;j++){//space
-            System.out.print(" ");
-         }
-         for(int k=1;k<=2*i-1;k++){
-            System.out.print("*");
-         }
-         System.out.print("\n");
+   public static void main(String[] args) throws InterruptedException {
+      A t=new A();
+      t.start();
 
-       }
-
-       for(int i=4;i>=1;i--)
-       {
-        for(int j=4;j>=i;j--)
-        {
-            System.out.print(" ");
-        }
-        for(int k=1;k<=2*i-1;k++)
-        {
-          System.out.print("*");
-        }
-        System.out.print("\n");
-       }
-    }
+      for(int i=1;i<=5;i++){
+         System.out.println("Zulfikar");
+         Thread.sleep(1000);
+      }
+   }
 }
