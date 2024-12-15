b3daab458df7f72e420a78a37ba9d8e4a1a68f64
hasanurislam <hasanur7811@gmail.com>
2024-12-15 14:14:49 +0530

    dimond pattern

--- Exam1.java
+++ Exam1.java
@@ -1,16 +1,29 @@
 public class Exam1 {
 
     public static void main(String[] args) {
-       for(int i=1;i<=5;i++)
+       for(int i=1;i<=5;i++)//rows
        {
-        for(int j=1;j<i;j++){
+         for(int j=i;j<5;j++){//space
+            System.out.print(" ");
+         }
+         for(int k=1;k<=2*i-1;k++){
+            System.out.print("*");
+         }
+         System.out.print("\n");
+
+       }
+
+       for(int i=4;i>=1;i--)
+       {
+        for(int j=4;j>=i;j--)
+        {
             System.out.print(" ");
         }
-        for(int k=i;k<=5;k++){
-            System.out.print("*");
+        for(int k=1;k<=2*i-1;k++)
+        {
+          System.out.print("*");
         }
-        System.out.println();
-
+        System.out.print("\n");
        }
     }
 }
